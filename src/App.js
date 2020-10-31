d4e0c041748713b151680ac59cdf01ec94aacdd0
duskull96 <pavel6900@gmail.com>
2020-10-31 17:03:26 +0300

    blabla

--- src/App.js
+++ src/App.js
@@ -1,15 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main';
+import FetchData from './service/FetchData'
+import Context from './context'
 
 
 import './style.scss'
 
-
 function App() {
-    return (
-        <div className='title'>
-            <h1>It's Weather App</h1>
-        </div>
-    );
+
+
+    const [cities, setCity] = useState([
+        // {
+        //     name: 'Москва',
+        //     main: {
+        //         temp: 20,
+        //         feels_like: 17,
+        //         humidity: 60
+        //     },
+        //     dt: null,
+        //     weather: [{
+        //         description: 'солнечно',
+        //         icon: null
+        //     }]
+        // },
+        // {
+        //     name: 'Санкт-Петербург',
+        //     main: {
+        //         temp: 15,
+        //         feels_like: 10,
+        //         humidity: 91
+        //     },
+        //     dt: null,
+        //     weather: [{
+        //         description: 'дождь',
+        //         icon: null
+        //     }]
+        // },
+        // {
+        //     name: 'Новосибирск',
+        //     main: {
+        //         temp: 19,
+        //         feels_like: 15,
+        //         humidity: 76
+        //     },
+        //     dt: null,
+        //     weather: [{
+        //         description: 'пасмурно',
+        //         icon: null
+        //     }]
+        // }
+    ])
+
+    const cache = []
+
+    function updateWeather(value) {
+        let weather = []
+        console.log(value, 'app', Date.now())
+        cache.push(value)
+        weather = (cache.pop())
+        console.log(weather, 'app')
+        setCity(cities.concat(weather))
+}
+    console.log(cities,'cities')
+return (
+    <Context.Provider>
+        <Header updateWeather={updateWeather} />
+        <Main city={cities} />
+    </Context.Provider>
+);
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+// import FetchData from '../../service/FetchData'
+import './header.scss'
+
+
+function Header(props) {
+    let [city, setCity] = useState('')
+    const [weather, setWeather] = useState({})
+
+    function submitHandler(event) {
+        event.preventDefault()
+        if (city.trim()) {
+            
+            // let startUrl = 'api.openweathermap.org/data/2.5/weather?lang=ru&appid=';
+            // let apiKey = '47765400ad97b7c36c3d970d3dc8f2d9';
+            // let url = startUrl + apiKey + '&q=' + city;
+            fetch('http://api.openweathermap.org/data/2.5/weather?lang=ru&appid=47765400ad97b7c36c3d970d3dc8f2d9&units=metric&q=' + city)
+                .then(response => response.json())
+                .then(commits => {
+                    setWeather(commits)
+                    setCity('')
+                    setWeather({})
+                })
+                // .then(() => event.target.children[0].value = '')
+                
+            // setTimeout(() => {
+            //     props.updateWeather(weather)
+            // }, 2000);
+            console.log(weather,'fetch');
+        }
+
+    }
+
+    // function updateDate(weather) {
+    //     if (JSON.stringify(weather) !== '{}') {
+    //         props.updateWeather(weather)
+    //     }  
+    // }
+
+    return (
+        <header className='header'>
+            {  (JSON.stringify(weather) === '{}' 
+            || JSON.stringify(weather) === JSON.stringify())? submitHandler : props.updateWeather(weather)}
+            <div className='__title'>
+                <h1>React Weather</h1>
+                <h3>введи название любого города мира</h3>
+            </div>
+            <div className='__form'>
+                <form onSubmit={submitHandler}>
+                    <input
+                        type='text'
+                        id='cityname'
+                        className='__cityname'
+                        autoComplete='on'
+                        value={city}
+                        onChange={event => setCity(event.target.value)}
+                    />
+                    <button  type='submit'>Добавить</button>
+                </form>
+            </div>
+            
+            <pre>{JSON.stringify(weather, ' ', 2)}</pre>
+        </header>
+    );
+};
+
+export default Header;
+
